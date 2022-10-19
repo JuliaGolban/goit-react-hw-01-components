1ef7562637251f9980e1a109127a5aff263a4cf7
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-19 22:47:00 +0300

    updated styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-export const Profile = ({ username, tag, location, avatar, followers, views, likes }) => {
+export const Profile = ({ avatar, username, tag, location, followers, views, likes }) => {
     return (<div className={css.profile}>
   <div className={css.description}>
     <img
