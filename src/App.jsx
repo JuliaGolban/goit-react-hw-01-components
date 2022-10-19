6b67229df120e3e89362c38a69b3061da7416339
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-19 22:54:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { Profile } from "components/Profile/Profile";
-
-import user from './components/Profile/user.json';
+import user from './data/user.json';
+import stats from './data/data.json';
 import css from 'App.module.css'
+import { Profile } from "./components/Profile/Profile";
+import { Statistics } from "./components/Statistics/Statistics";
 
 export const App = () => {
   return (
     <div className={css.App }>
       
       <Profile
+        avatar={user.avatar}
         username={user.username}
         tag={user.tag}
         location={user.location}
-        avatar={user.avatar}
         followers={user.stats.followers.toLocaleString('ru')}
         views={user.stats.views.toLocaleString('ru')}
         likes={user.stats.likes.toLocaleString('ru')}
       />
-      
+
+      <Statistics title="Upload stats" stats={stats} />
+      <Statistics stats={stats} />
+
     </div>
   );
 };
