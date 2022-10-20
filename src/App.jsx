61f9d5e5f42bd49d3da86affe4f998e5d1e2b7dc
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-20 14:57:11 +0300

    added 04-Transactions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import user from './data/user.json';
-import stats from './data/data.json';
-import friends from './data/friends.json';
 import css from 'App.module.css'
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import { Profile } from './components/Profile/Profile';
 import { Statistics } from './components/Statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from 'components/Transaction/TransactionHistory';
 
 export const App = () => {
   return (
@@ -20,10 +22,12 @@ export const App = () => {
         likes={user.stats.likes.toLocaleString('ru')}
       />
 
-      <Statistics title="Upload stats" stats={stats} />
-      <Statistics stats={stats} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
 
     </div>
   );
