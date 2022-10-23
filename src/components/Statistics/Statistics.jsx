e6cf82daa5932cdd6b9b878b509babeb514e75b5
JuliaGolban <julia.j.shcherban@gmail.com>
2022-10-23 21:25:39 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({title, stats}) => {
    
     return (<section className={css.statistics}>
-      { title ? (<h2 className={css.title}>{title}</h2>) : ''}
+      { title && (<h2 className={css.title}>{title}</h2>)}
         <ul className={css.stat_list}>
             {stats.map(({ id, label, percentage }) => {
                 return (<li className={css.item} key={id} style={{ backgroundColor: `#${Math.floor(Math.random() * 16225162).toString(16)}`}}>
